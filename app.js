4727a729798875efd4b4904ed1bf019319c494b6
Jellopy <parnupat.j@gmail.com>
2020-09-24 23:40:34 +0700

    test reply

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser'
+import request from 'request'
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.post('/webhook', (req, res) => res.sendStatus(200))
+const access_token = `rCzdtQar2QHfxY0WihWLJFlrJIY+EH5r9ItdktsFQec0PHfCn/1xG7iHGeMTSR12gABvgSamQ/ajAaj2K1nMRNe+ryd4Bv2FvqtEMLa/byUITkoXDYv2yWxABq3uOHmZwwSIAkAej7Khv3PnxnZBMwdB04t89/1O/w1cDnyilFU=`
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.post('/webhook', (req, res) => {
+  let reply_token = req.body.events[0].replyToken
+  reply(reply_token)
+  res.sendStatus(200)
+})
 app.use("*", (req, res) => {
      let text = "Hello world";
      return res.send(text);
@@ -10,3 +18,28 @@ app.use("*", (req, res) => {
 app.listen(PORT,() => {
   console.log(`server start port ${PORT}`);
 })
+
+function reply(reply_token) {
+  let headers = {
+      'Content-Type': 'application/json',
+      'Authorization': access_token
+  }
+  let body = JSON.stringify({
+      replyToken: reply_token,
+      messages: [{
+          type: 'text',
+          text: 'Hello'
+      },
+      {
+          type: 'text',
+          text: 'How are you?'
+      }]
+  })
+  request.post({
+      url: 'https://api.line.me/v2/bot/message/reply',
+      headers: headers,
+      body: body
+  }, (err, res, body) => {
+      console.log('status = ' + res.statusCode);
+  });
+}
