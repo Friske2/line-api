446eb036e6bc326c5460849f456858447f6b9e15
Jellopy <parnupat.j@gmail.com>
2020-09-24 23:29:06 +0700

    use express serve

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 import express from 'express';
-import http from 'http';
 const app = express();
 const PORT = process.env.PORT || 3000;
-http.createServer(app).listen(PORT, () => {
-     console.log("server status : running");
-     console.log(`run on port : ${PORT}`);
-});
+
 app.post('/webhook', (req, res) => res.sendStatus(200))
 app.use("*", (req, res) => {
      let text = "Hello world";
      return res.send(text);
+})
+app.listen(PORT,() => {
+  console.log(`server start port ${PORT}`);
 })
