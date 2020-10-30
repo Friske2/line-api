e0a6dbdba322d1e45b1e6b0052997289266a3d8b
Jellopy <parnupat.j@gmail.com>
2020-10-30 10:56:02 +0700

    test check in

--- app.js
+++ app.js
@@ -21,23 +21,56 @@ app.listen(PORT,() => {
 })
 
 function reply(reply_token,msg) {
-  let headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer {${access_token}}`
-  }
-  let body = JSON.stringify({
-      replyToken: reply_token,
-      messages: [{
+  if(msg === '#checkin') {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer {${access_token}}`
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
           type: 'text',
           text: msg
+        }
+        ]
+    })
+    let formValue = new FormData()
+    formValue.append('actionstate','checkin')
+    formValue.append('employee','checkin')
+    formValue.append('in_time','')
+    formValue.append('latitude','13.7456517')
+    formValue.append('longitude','100.5408732')
+    request.post({
+      url:'https://hr-terrabit-uat.demo1.webcontrol.com/app/branch1/api.php',
+      body: formValue
+    }, (err,res,body)=> {
+      try {
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: res
+        }, (err, res, body) => {
+          try {
+            console.log(res,body,err)
+          } catch (err) {
+            console.err
+          }
+            console.log('status = ' + res.statusCode);
+        });
+      } catch (error) {
+        request.post({
+          url: 'https://api.line.me/v2/bot/message/reply',
+          headers: headers,
+          body: error
+        }, (err, res, body) => {
+          try {
+            console.log(res,body,err)
+          } catch (err) {
+            console.err
+          }
+            console.log('status = ' + res.statusCode);
+        });
       }
-      ]
-  })
-  request.post({
-      url: 'https://api.line.me/v2/bot/message/reply',
-      headers: headers,
-      body: body
-  }, (err, res, body) => {
-      console.log('status = ' + res.statusCode);
-  });
+    })
+  }
 }
